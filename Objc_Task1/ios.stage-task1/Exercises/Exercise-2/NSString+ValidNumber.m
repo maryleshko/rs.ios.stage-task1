baaae5640115e12d94d0d057da36ec9a927ce560
Maryia Liashko <maryia.liashko@devexperts.com>
2024-09-11 12:42:11 +0200

    Added valid number task solution.

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -2,8 +2,20 @@
 
 @implementation NSString (ValidNumber)
 
++ (NSNumberFormatter *)numberFormatter {
+    static NSNumberFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSNumberFormatter alloc] init];
+        formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    });
+    return formatter;
+}
+
 - (BOOL)isValidNumber {
-    return false;
+    NSNumberFormatter *formatter = [NSString numberFormatter];
+    NSNumber *number = [formatter numberFromString:self];
+    return number != nil;
 }
 
 @end
